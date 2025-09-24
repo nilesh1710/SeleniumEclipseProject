232db9b0558cfcf50438945d27e42c77bf4c87a8
nilesh1710 <nshekdar@tiuconsulting.com>
2025-09-24 15:30:25 +0530

    Add files via upload

---
+++ src/test/java/TestSuite/TC34_Verifyinvalidloginattempts.java
@@ -0,0 +1,69 @@
+package TestSuite;
+
+import java.awt.AWTException;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.KredSafe.TestBase.TestBase;
+import com.KredSafe.User.ClientLoginPage;
+import com.KredSafe.User.RegisterPage;
+import com.relevantcodes.extentreports.LogStatus;
+
+import edu.emory.mathcs.backport.java.util.concurrent.TimeoutException;
+
+
+
+public class TC34_Verifyinvalidloginattempts extends TestBase {
+	public static final Logger log = Logger.getLogger(TC34_Verifyinvalidloginattempts.class.getName());
+
+	//@BeforeClass
+	//public static void startTest() {
+
+	//	test = extent.startTest("TC18_Restoreprofiledata");
+
+	//}
+	
+	@FindBy(xpath = "/html/body/div[3]/aside/div/div[2]/nav/ul/li[2]/a")
+	WebElement personalDetails;
+
+	@BeforeMethod
+	public void setup(Method result) throws IOException {
+		test = extent.startTest(result.getName());
+		test.log(LogStatus.INFO, result.getName() + " test Started");
+		init();
+		
+	
+	}
+	@Test(priority = 1, enabled = true)
+	public void Verifyloginattempt() throws IOException, InterruptedException, AWTException, TimeoutException {
+		
+
+		log.info("<===========Started Verifying RegisterUsingOnlineApplication===========> ");
+		openAppUrl();
+		ClientLoginPage clp = new ClientLoginPage(driver);
+		//clp.clientSignIn(OR.getProperty("emailID18"), OR.getProperty("createAccountPassword18"));
+		//clp.clientSignIn("sep9@yopmail.com", "Nilesh@20251");
+
+	//RegisterPage clp2 = new RegisterPage(driver);
+		wait_for_page_load(2);
+		//waitInSeconds(10);
+		//clp2.accessYopmaiforOTP();
+		//clp2.verifyOTPFunctionality(OR.getProperty("OTPSource"),OR.getProperty("clientSignIn"));
+		clp.Verifyfaillogin();
+		
+	}
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+		get_result(result);
+	}
+	}
+	
