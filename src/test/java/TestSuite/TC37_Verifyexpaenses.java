dd0a662e91c0d6ec2c72f3662e6370dc73a84934
nilesh1710 <nshekdar@tiuconsulting.com>
2025-11-10 13:34:14 +0530

    Add files via upload

---
+++ src/test/java/TestSuite/TC37_Verifyexpaenses.java
@@ -0,0 +1,71 @@
+package TestSuite;
+
+import java.awt.AWTException;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.KredSafe.TestBase.TestBase;
+import com.KredSafe.User.ClientLoginPage;
+import com.KredSafe.User.RegisterPage;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class TC37_Verifyexpaenses extends TestBase {
+	
+	public static final Logger log = Logger.getLogger(TC37_Verifyexpaenses.class.getName());
+	private static final String By = null;
+
+	//@BeforeClass
+	//public static void startTest() {
+
+	//	test = extent.startTest("TC23_VerifyPendingActivity");
+
+	//}	
+	@FindBy(xpath = "/html/body/div[3]/nav/ul[1]/li[2]/span/img")
+	WebElement menuclick;
+	
+	@FindBy(xpath = "/html/body/div[3]/aside/div/div[2]/nav/ul/li[6]/a")
+	WebElement formclick;
+	@BeforeMethod
+	public void setup(Method result) throws IOException {
+		test = extent.startTest(result.getName());
+		test.log(LogStatus.INFO, result.getName() + " test Started");
+		init();
+
+	}
+	
+	@Test(priority = 1, enabled = true)
+	public void Expenses() throws IOException, InterruptedException, AWTException {
+
+		log.info("<===========Started Verifying Expenses===========> ");
+		openAppUrl();
+		ClientLoginPage clp = new ClientLoginPage(driver);
+		//clp.clientSignIn(OR.getProperty("emailID3"), OR.getProperty("createAccountPassword3"));
+		clp.clientSignIn("sep10@yopmail.com", "Nilesh@2025");
+		RegisterPage rp = new RegisterPage(driver);
+		//rp.accessYopmaiforOTP();
+		//rp.verifyOTPFunctionality(OR.getProperty("OTPSource"), null);
+		waitInSeconds(10);
+		ClientLoginPage clp1 = new ClientLoginPage(driver);
+		clp1.expenses();
+		//RegisterPage clp2 = new RegisterPage(driver);
+	   // clp2.FormPlans();
+	
+	}
+
+
+	@AfterMethod	public void afterMethod(ITestResult result) {
+		get_result(result);
+
+	}
+
+
+}
